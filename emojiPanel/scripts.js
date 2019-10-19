63fa1d2f02440497fcb2ad95470b1ac71458ac5a
amaltr42 <amelia.travers@gmail.com>
2019-10-19 15:45:20 +1100

    animated all 5 emoji differently

--- emojiPanel/scripts.js
+++ emojiPanel/scripts.js
@@ -2,3 +2,19 @@ $("#cryLaugh").click(function() {
   console.log("click");
   $(this).toggleClass('animationCryLaugh');
 });
+$("#heartEyes").click(function() {
+  console.log("click");
+  $(this).toggleClass('animationHeartEyes');
+});
+$("#poop").click(function() {
+  console.log("click");
+  $(this).toggleClass('animationPoop');
+});
+$("#sad").click(function() {
+  console.log("click");
+  $(this).toggleClass('animationSad');
+});
+$("#shock").click(function() {
+  console.log("click");
+  $(this).toggleClass('animationShock');
+});
